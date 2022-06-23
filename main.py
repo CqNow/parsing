6e5806c271d193a3c3580a8d7c98e3c282661117
cqnow <cqnow1@gmail.com>
2022-06-23 19:12:12 +0300

    Recordpower catalog parser

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
+import img2pdf
 
 
 def get_data():
-    pass
+    img_list = []
+    """Скачиваем изображения с сайта"""
+    for i in range(1, 49):
+        response = requests.get(f'https://www.recordpower.co.uk/flip/Winter2020/files/mobile/{i}.jpg').content
+        """Сохраняем их"""
+        with open(f'data/{i}.jpg', 'wb') as file:
+            file.write(response)
+            img_list.append(f'data/{i}.jpg')
+            print(f'Download {i} of 48')
+    """Сохраняем в pdf"""
+    with open('data/recordpower.pdf', 'wb') as file:
+        file.write(img2pdf.convert(img_list))
 
+    print('PDF done')
 
 def main():
     get_data()
